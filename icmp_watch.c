4cf939bd21a46cfa757d515cd38eec254ffff980
zeGolem <zegolem1383@outlook.fr>
2022-08-14 22:24:21 +0200

    clang-format: Initial support
    Clang format is a popular tool for keeping consistant formatting in
    C/C++ projects. It can be ran using `clang-format icmp_watch.c` to see
    the formatted source code, and `clang-format -i icmp_watch.c` to apply
    the formatting changes. There are also plugins available for most
    IDEs/text editors to better integrate its features.
    
    I tried recreating a .clang-format config from reading the code, though
    it may not be 100% accurate, as some parts were inconsistant, so I
    choose whichever was the most used, or, in some cases, my personal
    preference, while trying to keep icmp_watch.c's diff as small as
    possible. You can find the full list of clang-format parameters in the
    documentation: https://clang.llvm.org/docs/ClangFormatStyleOptions.html
    
    Using clang-format will make it easier to keep a consistant coding
    style, even if there are multiple contributors to this project

--- icmp_watch.c
+++ icmp_watch.c
@@ -9,9 +9,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <termios.h>		// for tcsetattr()
-#include <netdb.h>		// for getattrinfo()
-#include <time.h>		// for nanosleep()
+#include <termios.h>	// for tcsetattr()
+#include <netdb.h>	// for getattrinfo()
+#include <time.h>	// for nanosleep()
 #include <sys/socket.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -25,62 +25,57 @@
 // To allow all users to use icmp sockets, run:
 // $ sysctl -w net.ipv4.ping_group_range="0 2147483647"
 
+#define RESETALL    "\e[0m"
 
-#define RESETALL        "\e[0m"
+#define CURSORHOME  "\e[H"
 
-#define CURSORHOME      "\e[H"
+#define CLEARSCREEN "\e[H\e[2J\e[3J"
 
-#define CLEARSCREEN     "\e[H\e[2J\e[3J"
+#define FGWHT	    "\e[1;37m"
 
-#define FGWHT		"\e[1;37m"
+#define BGRED	    "\e[1;41m"
 
-#define BGRED		"\e[1;41m"
+#define BGGRN	    "\e[1;42m"
 
-#define BGGRN		"\e[1;42m"
-
-
-static struct termios orig_termios;	// The terminal settings before we modified it.
-
+static struct termios orig_termios;    // The terminal settings before we modified it.
 
 void disableRawMode()
 {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-
 void enableRawMode()
 {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);				// Don't echo key presses.
-	raw.c_lflag &= ~(ICANON);			// Read by char, not by line.
-	raw.c_cc[VMIN] = 0;				// No minimum nr of chars.
-	raw.c_cc[VTIME] = 0;				// No waiting time.
+	raw.c_lflag &= ~(ECHO);	     // Don't echo key presses.
+	raw.c_lflag &= ~(ICANON);    // Read by char, not by line.
+	raw.c_cc[VMIN] = 0;	     // No minimum nr of chars.
+	raw.c_cc[VTIME] = 0;	     // No waiting time.
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-
-static int ping_all(int cnt, struct in_addr *destinations, int* response_times, struct timeval* timeout)
+static int ping_all(int cnt, struct in_addr* destinations, int* response_times, struct timeval* timeout)
 {
 	static int sequence = 0;
 	const int waittime = (int) (timeout->tv_sec * 1000000 + timeout->tv_usec);
-	int sock = socket(AF_INET,SOCK_DGRAM,IPPROTO_ICMP);
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 	if (sock < 0)
 	{
 		perror("socket");
 		exit(4);
 	}
 
-	for (int i=0; i<cnt; ++i)
-		response_times[i] = -1;	// what we return if we did not get a reply.
+	for (int i = 0; i < cnt; ++i)
+		response_times[i] = -1;	   // what we return if we did not get a reply.
 
 	// Send an ICMP request to each destination.
 	struct icmphdr icmp_hdr;
 	unsigned char txdata[256];
 	const int payloadsz = 10;
-	memcpy(txdata + sizeof icmp_hdr, "icmp_watch", payloadsz); //icmp payload
-	for (int i=0; i<cnt; ++i)
+	memcpy(txdata + sizeof icmp_hdr, "icmp_watch", payloadsz);    // icmp payload
+	for (int i = 0; i < cnt; ++i)
 	{
 		struct sockaddr_in addr;
 		memset(&addr, 0, sizeof addr);
@@ -93,23 +88,23 @@ static int ping_all(int cnt, struct in_addr *destinations, int* response_times, 
 
 		icmp_hdr.un.echo.sequence = sequence;
 		memcpy(txdata, &icmp_hdr, sizeof icmp_hdr);
-		int rc = sendto(sock, txdata, sizeof icmp_hdr + payloadsz, 0, (struct sockaddr*)&addr, sizeof addr);
+		int rc = sendto(sock, txdata, sizeof icmp_hdr + payloadsz, 0, (struct sockaddr*) &addr, sizeof addr);
 		if (rc <= 0)
 		{
 			perror("Sendto");
 			exit(3);
 		}
-        }
+	}
 	sequence++;
 
 	fd_set read_set;
-        memset(&read_set, 0, sizeof read_set);
-        FD_SET(sock, &read_set);
+	memset(&read_set, 0, sizeof read_set);
+	FD_SET(sock, &read_set);
 
-	int num_replies=0;
+	int num_replies = 0;
 	while (num_replies < cnt)
 	{
-		//wait for a reply with a timeout
+		// wait for a reply with a timeout
 		const int rc0 = select(sock + 1, &read_set, NULL, NULL, timeout);
 		if (rc0 == 0)
 		{
@@ -127,20 +122,20 @@ static int ping_all(int cnt, struct in_addr *destinations, int* response_times, 
 		struct sockaddr_in other_addr;
 		socklen_t other_addr_len = sizeof(other_addr);
 		const int rc1 = recvfrom(sock, rcdata, sizeof rcdata, 0, (struct sockaddr*) &other_addr, &other_addr_len);
-		if (rc1 <= 0) 
+		if (rc1 <= 0)
 		{
 			perror("recvfrom");
 			exit(5);
 		}
 		if (rc1 < sizeof rcv_hdr)
-			exit(6); // ICMP packet was too short.
+			exit(6);    // ICMP packet was too short.
 		const struct in_addr send_addr = other_addr.sin_addr;
-		int idx=-1;
+		int idx = -1;
 		// Look up which host sent us this reply.
-		for (int j=0; j<cnt; ++j)
+		for (int j = 0; j < cnt; ++j)
 			if (destinations[j].s_addr == send_addr.s_addr)
 				idx = j;
-		assert(idx>=0);
+		assert(idx >= 0);
 		const int timeleft = (int) (timeout->tv_sec * 1000000 + timeout->tv_usec);
 		response_times[idx] = waittime - timeleft;
 		memcpy(&rcv_hdr, rcdata, sizeof rcv_hdr);
@@ -153,7 +148,6 @@ static int ping_all(int cnt, struct in_addr *destinations, int* response_times, 
 	return num_replies;
 }
 
-
 // Resolves a set of hostnames to IPv4 numbers.
 static int get_ip_addresses(int cnt, char** hosts, struct in_addr* ips)
 {
@@ -161,7 +155,7 @@ static int get_ip_addresses(int cnt, char** hosts, struct in_addr* ips)
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_DGRAM;
-	for (int i=0; i<cnt; ++i)
+	for (int i = 0; i < cnt; ++i)
 	{
 		struct addrinfo* infos;
 		const char* host = hosts[i];
@@ -181,8 +175,7 @@ static int get_ip_addresses(int cnt, char** hosts, struct in_addr* ips)
 	return cnt;
 }
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
@@ -190,46 +183,46 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	const int cnt = argc-1;		// Every argument on command line is a hostname.
-	struct in_addr dst[cnt];	// The IP numbers of the hosts.
-	int response_times[cnt];	// The response time for each host we ping.
+	const int cnt = argc - 1;    // Every argument on command line is a hostname.
+	struct in_addr dst[cnt];     // The IP numbers of the hosts.
+	int response_times[cnt];     // The response time for each host we ping.
 
-	fprintf(stderr,"Looking up %d ip numbers...", cnt);
+	fprintf(stderr, "Looking up %d ip numbers...", cnt);
 	fflush(stderr);
-	const int num = get_ip_addresses(cnt, argv+1, dst);
-	fprintf(stderr,"DONE\n");
-	if (num!=cnt)
+	const int num = get_ip_addresses(cnt, argv + 1, dst);
+	fprintf(stderr, "DONE\n");
+	if (num != cnt)
 	{
-		fprintf(stderr,"Could not resolve all hostnames. Aborting.\n");
+		fprintf(stderr, "Could not resolve all hostnames. Aborting.\n");
 		exit(2);
 	}
 
-	enableRawMode();	// Don't echo keyboard characters, don't buffer them.
+	enableRawMode();    // Don't echo keyboard characters, don't buffer them.
 
-	int done=0;
-	while(!done)
+	int done = 0;
+	while (!done)
 	{
 		// When ESC or Q is pressed, we should terminate.
 		char c;
-		const int numr = read( STDIN_FILENO, &c, 1 );
-		if ( numr == 1 && ( c == 27 || c == 'q' || c == 'Q' ) )
+		const int numr = read(STDIN_FILENO, &c, 1);
+		if (numr == 1 && (c == 27 || c == 'q' || c == 'Q'))
 			done = 1;
-		struct timeval timeout = { 1, 0 }; // seconds, microseconds.
+		struct timeval timeout = {1, 0};    // seconds, microseconds.
 		ping_all(cnt, dst, response_times, &timeout);
 		fprintf(stdout, CLEARSCREEN);
-		for (int i=0; i<cnt; ++i)
+		for (int i = 0; i < cnt; ++i)
 		{
 			const int t = response_times[i];
-			fprintf(stdout,"%-20s", argv[1+i]);
-			if (t<0)
+			fprintf(stdout, "%-20s", argv[1 + i]);
+			if (t < 0)
 				fprintf(stdout, FGWHT BGRED "NO REPLY" RESETALL "\n");
 			else
-				fprintf(stdout, FGWHT BGGRN "%5d ms" RESETALL "\n", t/1000);
+				fprintf(stdout, FGWHT BGGRN "%5d ms" RESETALL "\n", t / 1000);
 		}
 		// Pace ourselves.
 		const int NS_PER_US = 1000;
 		const int us_left = (int) (timeout.tv_sec * 1000000 + timeout.tv_usec);
-		struct timespec ts = { 0, us_left * NS_PER_US };
+		struct timespec ts = {0, us_left * NS_PER_US};
 		nanosleep(&ts, 0);
 	}
 	fprintf(stdout, CLEARSCREEN);
@@ -237,4 +230,3 @@ int main(int argc, char *argv[])
 }
 
 // vi: tabstop=8
-
